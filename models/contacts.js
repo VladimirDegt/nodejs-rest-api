2dc7a6f0d9a27b6366d4d68ef17593fab0d9e081
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-08-01 11:58:56 +0300

    create operation getAll, getById, add

--- models/contacts.js
+++ models/contacts.js
@@ -1,12 +1,35 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises');
+const path = require('path');
+const { nanoid } = require('nanoid');
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, 'contacts.json');
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const getContacts = await fs.readFile(contactsPath);
+  return JSON.parse(getContacts);
+}
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const getContacts = await listContacts();
+  const oneContact = getContacts.find(item => item.id === contactId)
+  return oneContact || null
+}
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+}
+
+const addContact = async ({name, email, phone}) => {
+  const getContacts = await listContacts();
+  if(name && email && phone) {
+    const newContact = { id: nanoid(), name, email, phone };
+    getContacts.push(newContact);
+    console.table(getContacts);
+    fs.writeFile(contactsPath, JSON.stringify(getContacts, null, 2));
+
+    return newContact;
+  }
+  return null;
+}
 
 const updateContact = async (contactId, body) => {}
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,16 +2,29 @@ const express = require('express')
 
 const router = express.Router()
 
+const contacts = require('../../models/contacts')
+
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const getContacts = await contacts.listContacts();
+  res.json(getContacts)
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const oneContact = await contacts.getContactById(req.params.contactId)
+  if(oneContact) {
+    res.json(oneContact)
+    return
+  }
+  res.status(404).json({ message: 'Not found' })
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const addContact = await contacts.addContact(req.body)
+  if(addContact) {
+    res.status(201).json(addContact)
+    return
+  }
+  res.status(400).json({ message: 'missing required name field' })
 })
 
 router.delete('/:contactId', async (req, res, next) => {
