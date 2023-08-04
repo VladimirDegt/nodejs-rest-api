a4fef598e753749dd6c7be9e1e30ef719622b945
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-08-04 17:12:29 +0300

    add validate run server

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const { DB_HOST } = process.env;
 
 mongoose
   .connect(DB_HOST)
-  .then(() => console.log("Database connect"))
+  .then(() => {
+    console.log("Database connect");
+    app.listen(300);
+    console.log("Server running. Use our API on port: 3000");
+  })
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
   });
-
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
